5847db8f321f3b252d193eabd06fce9d1331d46b
Kareem Krayem <50558667+Kareem-Krayem@users.noreply.github.com>
2020-08-19 01:26:27 +0300

    Update firebase.js

--- messenger_clone/src/firebase.js
+++ messenger_clone/src/firebase.js
@@ -1,14 +1,8 @@
 import firebase from 'firebase';
 
+//here are the configurations that you need to put from firebase
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAnCE24THkOe5JsfEXLhZrHGQcsMWVSEsk",
-    authDomain: "kareem-messenger-clone.firebaseapp.com",
-    databaseURL: "https://kareem-messenger-clone.firebaseio.com",
-    projectId: "kareem-messenger-clone",
-    storageBucket: "kareem-messenger-clone.appspot.com",
-    messagingSenderId: "184491847756",
-    appId: "1:184491847756:web:dff1249f601b26476004e9",
-    measurementId: "G-KVS8XFWCGQ"
+
 });
 
 const db = firebaseApp.firestore();
