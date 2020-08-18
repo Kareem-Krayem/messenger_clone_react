018473183c0b5fa40f1800bce7e35d834eee2b58
Kareem Krayem <50558667+Kareem-Krayem@users.noreply.github.com>
2020-08-19 01:26:43 +0300

    Update firebase.js

--- messenger_clone/src/firebase.js
+++ messenger_clone/src/firebase.js
@@ -1,8 +1,7 @@
 import firebase from 'firebase';
 
-//here are the configurations that you need to put from firebase
 const firebaseApp = firebase.initializeApp({
-
+//here are the configurations that you need to put from firebase
 });
 
 const db = firebaseApp.firestore();
